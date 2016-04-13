a26ff3a425277a185abcb1247aa66c45809d3a29
Jiang Kui <jk44607880@gmail.com>
2016-04-13 14:55:32 +0200

    Basic view now returns minimal HTHM

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
-def home_page():
-	pass
+	#def home_page():
+	#	return HttpResponse()
+#def home_page(request):8/
+	#return HttpResponse('<html>')
+#def home_page(request):
+	#return HttpResponse('<html><title>To-Do lists</title>')
+def home_page(request):
+	return HttpResponse('<html><title>To-Do lists</title></html>')
