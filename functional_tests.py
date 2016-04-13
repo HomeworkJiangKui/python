590a743259a6eb982879f509d619fd926ac1b4a5
Jiang Kui <jk44607880@gmail.com>
2016-04-13 15:03:17 +0200

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-	#	self.browser.implicitly_wait(3)
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -17,29 +17,29 @@ class NewVisitorTest(unittest.TestCase):
 
 		#她注意到网页的标题和头部都包含“To-Do”这个词
 		self.assertIn('To-Do',self.browser.title)
-		#header_text = self.browser.find_element_by_tag_name('h1').text
-		#self.assertIn('To-Do',header_text)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do',header_text)
 		
 		#应用邀请她输入一个待办事项
-		#inputbox = self.browser.find_element_by_id('id_new_item')
-		#self.assertEqual(
-			#inputbox.get_attribute('placeholder'),
-			#'Enter a to-do item'
-		#)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
 		#她在一个文本框内输入了“Buy peacock feathers”
 		#伊利斯的爱好是使用家蝇做饵钓鱼
-		#inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys('Buy peacock feathers')
 
 		#她按回车建后，页面更新了
 		#待办事项表格中显示了“1：Buy peacock feathers”
-		#inputbox.send_keys(Key.ENTER)
+		inputbox.send_keys(Key.ENTER)
 		
-		#table=self.browser.find_element_by_id('id_list_table')
-		#rows=table.find_elements_by_tag_name('tr')
-		#self.assertTrue(
-			#any(row.text == '1:Buy peacock feathers' for row in rows)
-		#)
+		table=self.browser.find_element_by_id('id_list_table')
+		rows=table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1:Buy peacock feathers' for row in rows)
+		)
 
 		#页面中有显示了一个文本框，可以输入其他的待办事项
 		#她输入了‘Use peacork feathers to make a fly“（使用孔雀羽毛做家蝇）
