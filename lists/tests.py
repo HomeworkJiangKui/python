3a7c2c350b942801dd060c6163bf07b4dffd22ca
Jiang Kui <jk44607880@gmail.com>
2016-04-19 20:29:56 +0200

    加入新的测试redirects

--- lists/tests.py
+++ lists/tests.py
@@ -16,6 +16,7 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(),expected_html)
+
 	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
 		request.method = 'POST'
@@ -29,7 +30,16 @@ class HomePageTest(TestCase):
 
 		self.assertEqual(response.status_code,302)
 		self.assertEqual(response['location'],'/')
-		#self.assertEqual(response.content.decode(),expected_html)
+
+	def test_home_page_redirects_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertEqual(response.status_code,302)
+		self.assertEqual(response['location'],'/')
 
 	def test_home_page_only_saves_items_when_necessary(self):
 		request = HttpRequest()
