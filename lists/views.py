607c60da7e1cb0fab37f813e97250e5f07fa865b
Jiang Kui <jk44607880@gmail.com>
2016-04-19 20:21:56 +0200

    简化测试，加入POST请求重定向操作

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
-	#def home_page():
-	#	return HttpResponse()
-#def home_page(request):8/
-	#return HttpResponse('<html>')
-#def home_page(request):
-	#return HttpResponse('<html><title>To-Do lists</title>')
-#def home_page(request):
-#	return HttpResponse('<html><title>To-Do lists</title></html>')
+from django.shortcuts import render,redirect
+from lists.models import Item
+
 def home_page(request):
-	return render(request,'home.html',{
-		'new_item_text':request.POST.get('item_text','')
-	})
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+
+	return render(request,'home.html')
