d145bb017c42b66f769e6d5b9d9755f50be6cedd
Jiang Kui <jk44607880@gmail.com>
2016-04-19 17:38:12 +0200

    加入了POST请求

--- functional_tests.py
+++ functional_tests.py
@@ -35,11 +35,15 @@ class NewVisitorTest(unittest.TestCase):
 		#待办事项表格中显示了“1：Buy peacock feathers”
 		inputbox.send_keys(Keys.ENTER)
 		
+		#import time
+		#time.sleep(10)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
 			any(row.text == '1:Buy peacock feathers' for row in rows),
-		"New to-do item did not appear in table"
+			"New to-do item did not appear in table -- its text was:\n%s" %(
+				table.text,
+			)
 		)
 
 		#页面中有显示了一个文本框，可以输入其他的待办事项
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-#from django.http import HttpResponse
 # Create your views here.
 	#def home_page():
 	#	return HttpResponse()
@@ -10,4 +10,6 @@ from django.shortcuts import render
 #def home_page(request):
 #	return HttpResponse('<html><title>To-Do lists</title></html>')
 def home_page(request):
-	return render(request,'home.html')
+	return render(request,'home.html',{
+		'new_item_text':request.POST.get('item_text','')
+	})
